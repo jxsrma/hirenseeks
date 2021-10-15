7951ee74d37a21e2fd71ec1c363444f24bea1c35
Jash Sharma <jashsharma619@gmail.com>
2021-10-16 00:52:00 +0530

    sample data column

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# class userData(models.Model):
+#     userName = models.CharField(max_length=100)
+#     firstName = models.CharField(max_length=50)
+#     lastName = models.CharField(max_length=50)
+#     email = models.CharField(max_length=70)
+#     contNum = models.IntegerField(max_length=10)
+#     dob =  models.DateField()
+#     address = models.CharField(max_length=100)
+#     city = models.CharField(max_length=50)
+#     state = models.CharField(max_length=50)
+#     country = models.CharField(max_length=50)
+    
+
