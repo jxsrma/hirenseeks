e89d44eaa414a221fb3b09d44538197b760af905
Jash Sharma <jashsharma619@gmail.com>
2021-10-19 20:33:04 +0530

    userdata db created

--- backend/models.py
+++ backend/models.py
@@ -3,30 +3,39 @@ from django.db import models
 # Create your models here.
 
 class userData(models.Model):
-    # userName = models.CharField(max_length=100)
-    # password = models.TextField(blank=True)
-    # firstName = models.CharField(max_length=50)
-    # lastName = models.CharField(max_length=50)
-    # email = models.EmailField(max_length=70)
-    # countryCode = models.CharField(max_length=5)
-    # contactNumber = models.IntegerField(max_length=10)
-    # dob =  models.DateField()
-    # address = models.CharField(max_length=100)
-    # city = models.CharField(max_length=50)
-    # state = models.CharField(max_length=50)
-    # country = models.CharField(max_length=50)
-    # #profilePic = models.blob
-    # #coverPic = models.blob
-    # bio = models.CharField(max_length=300)
-    # skills = models.JSONField(default = '{"skills" : [] }')
-    # projects = models.JSONField(default = '{"projects" : [] }')
-    # linkGithub = models.TextField(blank=True)
-    # linkLinkedIn = models.TextField(blank=True)
-    # linkExtra = models.JSONField(default = '{"links" : [] }')
+    userName = models.CharField(max_length=100)
+    password = models.TextField(blank=True)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70)
+    countryCode = models.CharField(max_length=5)
+    contactNumber = models.CharField(max_length=10)
+    dob =  models.DateField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    #profilePic = models.blob
+    #coverPic = models.blob
+    bio = models.CharField(max_length=300)
+    skills = models.JSONField(default = '{"skills" : [] }')
+    projects = models.JSONField(default = '{"projects" : [] }')
+    linkGithub = models.TextField(blank=True)
+    linkLinkedIn = models.TextField(blank=True)
+    linkExtra = models.JSONField(default = '{"links" : [] }')
     
-    # #**********************************************************
-    # dateJoined = models.DateField()
-    # lastLogin = models.DateField()
+    #**********************************************************
+    dateJoined = models.DateField()
+    #lastLogin = models.DateField()
 
 
 
+
+# 1. Delete your migrations files in your desired app
+# 2. Thanks to raul answer: In the database: DELETE FROM django_migrations WHERE app = 'app_name'.
+# 3. comment codes in models.py and all this models usage in views, signals and etc (to prevent error).
+# 4. python manage.py makemigrations YOUR_APP_NAME
+# 5. python manage.py migrate --fake
+# 6. un-comment what you commented in step 3
+# 7. python manage.py makemigrations YOUR_APP_NAME
+# 8. migrate without --fake: python manage.py migrate
