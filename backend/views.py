6ec540e3b95b60178e14297c47c9078a74067f57
Jash Sharma <jashsharma619@gmail.com>
2021-10-19 20:34:07 +0530

    User Successfully Created / Sign Up success

--- backend/views.py
+++ backend/views.py
@@ -3,6 +3,9 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt # import
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import User, auth
+from .models import userData
+from datetime import date
 
 @csrf_exempt
 def login(request):
@@ -10,26 +13,53 @@ def login(request):
         
         jsonData = json.loads(request.body)
 
-        username = jsonData['username']
-        firstN = jsonData['first_name']
-        lastN = jsonData['last_name']
-        eMail = jsonData['email']
+        user_name = jsonData['username']
+        first_N = jsonData['first_name']
+        last_N = jsonData['last_name']
+        e_Mail = jsonData['email']
+        dofb = jsonData['dob']
         contact = jsonData['contact']
-        password = make_password(jsonData['password'])
+        pass_word = make_password(jsonData['password'])
 
-        userInfo = {
-            'User Name' : username,
-            'First Name' : firstN,
-            'Last Name' : lastN,
-            'E-Mail' : eMail,
-            'Contact' : contact,
-            'Encrypted Password' : password
-        }
-        print(userInfo)
-        
-        
+        # demoData = Demosubs(a_name = artname, a_email = email, t_name = trackname, t_url = trackurl, t_dis = infotext )
+        # demoData.save()
+        # signUpData = userData()
+        if userData.objects.filter(userName = user_name).exists():
+            print("Username Taken")
+        # else:
+        #     signInData = userData(
+        #         userName = user_name,
+        #         password = pass_word,
+        #         firstName = first_N,
+        #         lastName = last_N,
+        #         email = e_Mail,
+        #         dob = dofb,
+        #         contactNumber = contact,
+        #         dateJoined = date.today()
+        #         )
+        #     signInData.save();
 
-        return JsonResponse(userInfo)
+            user = User.objects.create_user(
+                password = pass_word,
+                username = user_name,
+                first_name = first_N,
+                last_name = last_N,
+                email = e_Mail
+            )
+            user.save()
+
+            userInfo = {
+                'User Name' : user_name,
+                'First Name' : first_N,
+                'Last Name' : last_N,
+                'E-Mail' : e_Mail,
+                'Date of Birth' : dofb,
+                'Contact' : contact,
+                'Encrypted Password' : pass_word
+            }
+            print(userInfo)
+            print("Data Saved")
+            return JsonResponse(userInfo)
     else:
         print('Error')
         return "Error"
