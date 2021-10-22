5c18e01558e7cce94aaed1167b5338eb68b3dcf1
Jash Sharma <jashsharma619@gmail.com>
2021-10-22 14:51:43 +0530

    🤞User Login with Username/Email/Contact Working🤞

--- backend/views.py
+++ backend/views.py
@@ -1,17 +1,18 @@
 import json
 from django.http.response import JsonResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt # import
+from django.views.decorators.csrf import csrf_exempt  # import
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.models import User, auth
 from .models import userData
 from datetime import date
 
+
 @csrf_exempt
 def signup(request):
     if request.method == 'POST':
-        
+
         jsonData = json.loads(request.body)
 
         user_name = jsonData['username']
@@ -22,47 +23,47 @@ def signup(request):
         contact = jsonData['contact']
         pass_word = jsonData['password']
 
-        if User.objects.filter(username = user_name).exists():
+        if User.objects.filter(username=user_name).exists():
             print("Username Taken")
             return JsonResponse({
                 "success": False,
-                "error" : "User name Taken"
+                "error": "User name Taken"
             })
-        elif User.objects.filter(email = e_Mail).exists():
+        elif User.objects.filter(email=e_Mail).exists():
             print("Email already Exist")
             return JsonResponse({
                 "success": False,
-                "error" : "Email already Exist"
+                "error": "Email already Exist"
             })
         else:
             user = User.objects.create_user(
-                password = pass_word,
-                username = user_name,
-                first_name = first_N,
-                last_name = last_N,
-                email = e_Mail
+                password=pass_word,
+                username=user_name,
+                first_name=first_N,
+                last_name=last_N,
+                email=e_Mail
             )
             user.save()
 
             signInData = userData(
-                userName = user_name,
-                firstName = first_N,
-                lastName = last_N,
-                email = e_Mail,
-                dob = dofb,
-                contactNumber = contact,
-                dateJoined = date.today()
-                )
-            signInData.save();
+                userName=user_name,
+                firstName=first_N,
+                lastName=last_N,
+                email=e_Mail,
+                dob=dofb,
+                contactNumber=contact,
+                dateJoined=date.today()
+            )
+            signInData.save()
 
             userInfo = {
-                'User Name' : user_name,
-                'First Name' : first_N,
-                'Last Name' : last_N,
-                'E-Mail' : e_Mail,
-                'Date of Birth' : dofb,
-                'Contact' : contact,
-                'Encrypted Password' : pass_word
+                'User Name': user_name,
+                'First Name': first_N,
+                'Last Name': last_N,
+                'E-Mail': e_Mail,
+                'Date of Birth': dofb,
+                'Contact': contact,
+                'Encrypted Password': pass_word
             }
             print(userInfo)
             print("Data Saved")
@@ -70,38 +71,92 @@ def signup(request):
     else:
         print('Error')
         return JsonResponse({
-                "success": False,
-                "error" : "Unknown error"
-            })
+            "success": False,
+            "error": "Unknown error"
+        })
+
+
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
         jsonData = json.loads(request.body)
-        user = authenticate(username = jsonData['userName'],password = jsonData['password'])
-        if user is not None:
-            request.session['user'] = jsonData['userName']
-            return JsonResponse({
-            "Success" : True,
-            "Error" : "Login Success"
-        })
-        else:    
-            return JsonResponse({
-            "Success" : False,
-            "Error" : "Username or Password Wrong"
-        })
+        if "userName" in jsonData.keys():
+            
+            user = authenticate(
+                username=jsonData['userName'],
+                password=jsonData['password']
+            )
+            
+            if user is not None:
+                request.session['user'] = jsonData['userName']
+                return JsonResponse({
+                    "Success": True,
+                    "Error": "Login Success",
+                })
+            else:
+                return JsonResponse({
+                    "Success": False,
+                    "Error": "Username or Password Wrong"
+                })
+        elif "email" in jsonData.keys():
+            try:
+                userInfoByEmail = User.objects.get(email = jsonData['email'])
+            except:
+                return JsonResponse({"Error": "No User Found"})    
+            
+            print(userInfoByEmail.username)
+            user = authenticate(
+                username=userInfoByEmail.username,
+                password=jsonData['password']
+            )
+            
+            if user is not None:
+                request.session['user'] = userInfoByEmail.username
+                return JsonResponse({
+                    "Success": True,
+                    "Error": "Login Success",
+                })
+            else:
+                return JsonResponse({
+                    "Success": False,
+                    "Error": "Email or Password Wrong"
+                })
+        else:
+            try:
+                userInfoByCont = userData.objects.get(contactNumber = jsonData['contactNumber'])
+            except:
+                return JsonResponse({"Error": "Cont No User Found"})
+            user = authenticate(
+                username=userInfoByCont.userName,
+                password=jsonData['password'])
+            if user is not None:
+                request.session['user'] = userInfoByCont.userName
+                return JsonResponse({
+                    "Success": True,
+                    "Error": "Login Success"
+                })
+            else:
+                return JsonResponse({
+                    "Success": False,
+                    "Error": "Contact Number or Password Wrong"
+                })
+
     else:
         return JsonResponse({
-            "Success" : False,
-            "Error" : "No Request"
+            "Success": False,
+            "Error": "No Request"
         })
+
+
 @csrf_exempt
 def user(request):
     return JsonResponse({
-        "User" : request.session.get('user')
+        "User": request.session.get('user')
     })
+
 @csrf_exempt
 def logouts(request):
     logout(request)
     return JsonResponse({
-        "User" : "None"
+        "User": "None"
     })
