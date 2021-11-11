664c08a062ab5a6faa87d5a7fac1b52e87a31fc4
Jash Sharma <jashsharma619@gmail.com>
2021-11-11 23:48:41 +0530

    New Model Classes

--- backend/models.py
+++ backend/models.py
@@ -22,37 +22,36 @@ class userData(models.Model):
     linkGithub = models.TextField(blank=True)
     linkLinkedIn = models.TextField(blank=True)
     linkExtra = models.JSONField(default = '{"links" : [] }')
-    
-    #**********************************************************
     dateJoined = models.DateField()
     #lastLogin = models.DateField()
 
-# class postedJob(models.Model):
-#     title = models.CharField(max_length=50)
-#     jobPos = models.CharField(max_length=50)
-#     desc = models.CharField(max_length=50)
-#     timing = models.CharField(max_length=50)
-#     reqSkill = models.CharField(max_length=50)
-#     expLevel = models.CharField(max_length=50)
-#     postedBy = models.CharField(max_length=50)
-#     location = models.CharField(max_length=50)
+class postedJob(models.Model):
+    title = models.CharField(max_length=50)
+    jobPos = models.CharField(max_length=50)
+    desc = models.CharField(max_length=50)
+    timing = models.CharField(max_length=50)
+    reqSkill = models.JSONField(default = '{"skills" : [] }')
+    expLevel = models.CharField(max_length=50)
+    postedBy = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    appliedPeople = models.JSONField(default = '{"ID" : [] }')
 
-# class compData(models.Model):
-#     userName = models.CharField(max_length=100)
-#     compName = models.CharField(max_length=50)
-#     email = models.EmailField(max_length=70)
-#     countryCode = models.CharField(max_length=5)
-#     contactNumber = models.CharField(max_length=10)
-#     address = models.CharField(max_length=100)
-#     city = models.CharField(max_length=50)
-#     state = models.CharField(max_length=50)
-#     country = models.CharField(max_length=50)
-#     #profilePic = models.blob
-#     #coverPic = models.blob
-#     bio = models.CharField(max_length=300)
-#     linkGithub = models.TextField(blank=True)
-#     linkLinkedIn = models.TextField(blank=True)
-#     linkExtra = models.JSONField(default = '{"links" : [] }')
+class compData(models.Model):
+    userName = models.CharField(max_length=100)
+    compName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70)
+    countryCode = models.CharField(max_length=5)
+    contactNumber = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    #profilePic = models.blob
+    #coverPic = models.blob
+    bio = models.CharField(max_length=300)
+    linkGithub = models.TextField(blank=True)
+    linkLinkedIn = models.TextField(blank=True)
+    linkExtra = models.JSONField(default = '{"links" : [] }')
 
 # 1. Delete your migrations files in your desired app
 # 2. Thanks to raul answer: In the database: DELETE FROM django_migrations WHERE app = 'app_name'.
